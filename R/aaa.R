c7fe2f6c1eacb6119c29d926e3a4f7e335c43181
Aaron A. King <kingaa@umich.edu>
2018-12-13 13:13:28 -0500

    add banner message

--- R/aaa.R
+++ R/aaa.R
@@ -2,6 +2,14 @@
   exampleDir <- getOption("pomp.examples")
   pompExampleDir <- system.file("examples",package="pomp")
   options(pomp.examples=c(exampleDir,pompExampleDir,recursive=TRUE))
+  packageStartupMessage("Welcome to pomp version 1.19!\n",
+    "pomp version 2 is now available on https://kingaa.github.io/pomp/.\n",
+    "This new version contains many improvements but is not backward compatible.\n",
+    "To allow users time to modify their codes, 'pomp' version 2 will be provided as package 'pomp2' for a short while.\n\n",
+    "Around the middle of 2019, 'pomp2' will be renamed 'pomp' version 2.1 and will be released to CRAN. ",
+    "Until then, 'pomp' versions < 2 will continue to be available.\n\n",
+    "See the ",sQuote("pomp version 2 upgrade guide")," at ",
+    "https://kingaa.github.io/pomp/",".")
 }
 
 .onDetach <- function (...) {
